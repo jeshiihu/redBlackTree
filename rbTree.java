24634308f8501fc1828be15d32c5ca38dd4c485f
Jessica Huynh <jeshiihu@gmail.com>
2015-11-10 14:34:26 -0700

    adding some deletion

--- rbTree.java
+++ rbTree.java
@@ -6,18 +6,18 @@ public class rbTree {
 	public rbTree() { // create an empty tree
 		root = null;
 	}
-	public boolean search(int key) {
+	public node search(int key) {
 		node current = root;
 		while(current != null) {
-			if(current.key == key) return true;
+			if(current.key == key) return current;
 			else if(current.key > key) current = current.l;
 			else current = current.r;
 		}
-		return false;
+		return current;
 	}
 	
 	void insert(int key) {
-		if (search(key)) return; // do nothing if duplicate
+		if (search(key) != null) return; // do nothing if duplicate
 		else insert(new node(key));
 	}
 	private void insert(node insertNode) {
@@ -74,6 +74,15 @@ public class rbTree {
 		}
 		insertCase5(n);
 	}
+	
+	private void insertCase5(node n) { // red parent, black uncle
+		// left of left or right of right (outer)
+		n.p.c = Colour.BLACK;
+		n.g(n).c = Colour.RED;
+		if(n == n.p.l) rotateR(n.g(n));
+		else rotateL(n.g(n));
+	}
+	
 	private void rotateL(node n) {
 		node savedL = n.r.l;
 		if (n == root) {
@@ -102,14 +111,109 @@ public class rbTree {
 		n.l = savedR;
 		if (savedR != null) savedR.p = n;
 	}
-	
-	private void insertCase5(node n) { // red parent, black uncle
-		// left of left or right of right (outer)
-		n.p.c = Colour.BLACK;
-		n.g(n).c = Colour.RED;
-		if(n == n.p.l) rotateR(n.g(n));
-		else rotateL(n.g(n));
+	void delete(int key) {
+		if(search(key) == null) return; //tree does not contain this key, do nothing
+		node removeNode = search(key);
+		if(removeNode.r==null && removeNode.l==null) { // node to be removed  has no children
+			if(removeNode.p.l==removeNode) removeNode.p.l = null;
+			if(removeNode.p.r==removeNode) removeNode.p.r = null;
+			return;
+		}
+		deleteOneChild(removeNode);
 	}
+	void deleteOneChild(node n) {
+		node child = (n.r != null) ? n.l: n.r; //if n.r exists, return l, else return r
+		if(n.p.l==n) n.p.l = child;
+		if(n.p.r==n) n.p.r = child;
+		if(n.c == Colour.BLACK) {
+			if(child.c == Colour.RED) child.c = Colour.BLACK;
+			else deleteCase1(child);
+		}
+	}
+	void deleteCase1(node n) { // N is new root
+//		 if (n->parent != NULL)
+//			  delete_case2(n);
+	}
+//	void delete_case2(struct node *n)
+//	{
+//	 struct node *s = sibling(n);
+//
+//	 if (s->color == RED) {
+//	  n->parent->color = RED;
+//	  s->color = BLACK;
+//	  if (n == n->parent->left)
+//	   rotate_left(n->parent);
+//	  else
+//	   rotate_right(n->parent);
+//	 }
+//	 delete_case3(n);
+//	}
+//	void delete_case3(struct node *n)
+//	{
+//	 struct node *s = sibling(n);
+//
+//	 if ((n->parent->color == BLACK) &&
+//	     (s->color == BLACK) &&
+//	     (s->left->color == BLACK) &&
+//	     (s->right->color == BLACK)) {
+//	  s->color = RED;
+//	  delete_case1(n->parent);
+//	 } else
+//	  delete_case4(n);
+//	}
+//	void delete_case4(struct node *n)
+//	{
+//	 struct node *s = sibling(n);
+//
+//	 if ((n->parent->color == RED) &&
+//	     (s->color == BLACK) &&
+//	     (s->left->color == BLACK) &&
+//	     (s->right->color == BLACK)) {
+//	  s->color = RED;
+//	  n->parent->color = BLACK;
+//	 } else
+//	  delete_case5(n);
+//	}
+//	void delete_case5(struct node *n)
+//	{
+//	 struct node *s = sibling(n);
+//
+//	 if  (s->color == BLACK) { /* this if statement is trivial,
+//	due to case 2 (even though case 2 changed the sibling to a sibling's child,
+//	the sibling's child can't be red, since no red parent can have a red child). */
+//	/* the following statements just force the red to be on the left of the left of the parent,
+//	   or right of the right, so case six will rotate correctly. */
+//	  if ((n == n->parent->left) &&
+//	      (s->right->color == BLACK) &&
+//	      (s->left->color == RED)) { /* this last test is trivial too due to cases 2-4. */
+//	   s->color = RED;
+//	   s->left->color = BLACK;
+//	   rotate_right(s);
+//	  } else if ((n == n->parent->right) &&
+//	             (s->left->color == BLACK) &&
+//	             (s->right->color == RED)) {/* this last test is trivial too due to cases 2-4. */
+//	   s->color = RED;
+//	   s->right->color = BLACK;
+//	   rotate_left(s);
+//	  }
+//	 }
+//	 delete_case6(n);
+//	}
+//	void delete_case6(struct node *n)
+//	{
+//	 struct node *s = sibling(n);
+//
+//	 s->color = n->parent->color;
+//	 n->parent->color = BLACK;
+//
+//	 if (n == n->parent->left) {
+//	  s->right->color = BLACK;
+//	  rotate_left(n->parent);
+//	 } else {
+//	  s->left->color = BLACK;
+//	  rotate_right(n->parent);
+//	 }
+//	}
 	
 	void isEmpty() {
 		if (root == null) System.out.println("BST is empty.");
@@ -152,6 +256,7 @@ public class rbTree {
 			tree.printTree();
 			System.out.println("");
 			tree.clearTree();
+			
 	}
 }
 
@@ -186,5 +291,9 @@ class node {
 	 if (n.p == gp.l) return gp.r;
 	 else return gp.l;
 	}
+	
+	public node s(node n) {
+		if(n == n.p.l) return n.p.r;
+		else return n.p.l;
+	}
 }
-
